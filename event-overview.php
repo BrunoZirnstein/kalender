9ea25a4f9f58787abe8807b7d139dd1d7f839348
Bruno Zirnstein <zirnstein.bruno@gmail.com>
2020-02-18 11:08:26 +0100

    Überschrift hinzugefügt

--- event-overview.php
+++ event-overview.php
@@ -1,1 +1,11 @@
-<h1>Hallo</h1>
+<!DOCTYPE html />
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link href="event-overview-styles.css" />
+    </head>
+    <body>
+        <h1>Events</h1>
+
+    </body>
+</html>
