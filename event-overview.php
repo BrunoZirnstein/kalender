d45e9eeb23ae0f086774899c0f04c260b7a62780
Bruno Zirnstein <zirnstein.bruno@gmail.com>
2020-02-20 15:10:33 +0100

    event-overview it farben

--- event-overview.php
+++ event-overview.php
@@ -26,6 +26,23 @@
 
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()){
+
+                $event_sql = "SELECT * FROM `participant` JOIN event ON participant.eid = event.eid WHERE participant.uid = ".$_SESSION["user"];
+                $event_result = $link->query($event_sql);
+
+                if($event_result->num_rows > 0){
+                    $available = true;
+                    while($teilnehmer = $event_result->fetch_assoc()){
+                        if(strtotime($row["start"]) >= strtotime($teilnehmer["start"]) && strtotime($row["start"]) <= strtotime($teilnehmer["finish"])){
+                            $available = false;
+                        }
+
+                        if(strtotime($row["finish"]) >= strtotime($teilnehmer["start"]) && strtotime($row["finish"]) <= strtotime($teilnehmer["finish"])){
+                            $available = false;
+                        }
+                    }
+                }
+
                 $count = 0;
                 $participant_sql = "SELECT * FROM participant WHERE eid = ".$row["eid"];
                 $participant_result = $link->query($participant_sql);
@@ -35,14 +52,21 @@
                         $count++;
                     }
                 }
-                echo '<a class="itwc" href="event-detail.php?eid='.$row["eid"].'"><div class="itwc event">';
+
+                if($available == false){
+                    $class = ' red';
+                } else{
+                    $class = ' green';
+                }
+
+                echo '<a class="itwc" href="event-detail.php?eid='.$row["eid"].'"><div class="itwc event'.$class.'">';
                     echo $row["title"];
                     echo '<div class="right">';
                         echo '<div style="margin-right: 10px;">'.$count.'/'.$row["size"].'</div>';
                         ?>
                         <form style="margin: 0;" method="post" action="teilnehmen.php">
                             <input type="hidden" name="eid" value="<?php echo $row["eid"] ?>" />
-                            <input style="margin: 0;" type="submit" value="Teilnehmen" />
+                            <input style="margin: 0;" type="submit" value="Teilnehmen" <?php if($available == false){echo ' disabled';} ?> />
                         </form>
 
                         <?php
